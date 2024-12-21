cebb0bba84dee73d70ee4be012d8583781a8b037
sebas777 <sebastiancast06@hotmail.com>
2024-12-21 11:28:56 -0500

    [sc] se crea estructura del frontend, se instala bootstrap

---
+++ frontend/ClientApp/src/Components/NavBar.jsx
@@ -0,0 +1,13 @@
+const  NavBar = ({brand}) => {
+    return (  
+        <nav className="navbar navbar-dark bg-dark">
+            <div className="container">
+                <a href="!#" className="navbar-brand">{brand}</a>
+            </div>
+
+        </nav>
+
+    );
+}
+ 
+export default NavBar;
